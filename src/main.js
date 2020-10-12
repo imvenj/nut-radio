531d578b0a10fbbdb82b70c14e504ea7c54c2a81
Venj <ersaclarke@gmail.com>
2020-10-12 23:10:34 +0800

    Nutui components setup.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import NutUI from '@nutui/nutui'
 import '@nutui/nutui/dist/nutui.scss'
+import './assets/rem'
+import './assets/reset.scss'
+
 NutUI.install(Vue)
 
 Vue.config.productionTip = false
